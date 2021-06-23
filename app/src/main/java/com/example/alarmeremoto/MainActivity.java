5afec2b26f909edbd03e853ca3fa4b8cf95f8c2f
Davi Wei Tokikawa <davi.tokikawa@iotag.com.br>
2021-06-22 23:20:16 -0300

    Início do layout da tela principal; Lógica de teste do botão "Adicionar" alterando o texto.

--- app/src/main/java/com/example/alarmeremoto/MainActivity.java
+++ app/src/main/java/com/example/alarmeremoto/MainActivity.java
@@ -1,14 +1,46 @@
+/*
+    Trabalho de Conclusao de Curso 2021
+    Electronics Engineering - UTFPR
+
+    ALARME REMOTO
+
+    Alunos:
+    - Adriano Ricardo de Abreu Gamba
+    - Davi Wei Tokikawa
+    - Erika Maria Capote Both
+ */
+
 package com.example.alarmeremoto;
 
 import androidx.appcompat.app.AppCompatActivity;
+import android.widget.Button;
+import android.view.View;
+import android.widget.TextView;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView textView;
+    Button buttonAdd;
+    Button buttonRefresh;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = (TextView) findViewById(R.id.textView);
+        buttonAdd = findViewById(R.id.button1);
+        buttonRefresh = findViewById(R.id.button2);
+
+        buttonAdd.setOnClickListener(new View.OnClickListener() {
+            int counter = 0;
+            public void onClick(View v) {
+                // Code here executes on main thread after user presses button
+                textView.setText(counter % 2 == 0 ? "ALARME REMOTO" : "GAIOLA DE JAVALI");
+                counter++;
+            }
+        });
     }
 }
